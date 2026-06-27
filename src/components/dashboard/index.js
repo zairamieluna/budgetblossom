8f792b426d8a8df809a3c7070030f2add1de2e56
zairamieluna <zairamie.luna@gmail.com>
2026-06-26 21:31:47 -0400

    Create index.js

---
+++ src/components/dashboard/index.js
@@ -0,0 +1,8 @@
+export { default as DashboardHeader } from "./DashboardHeader";
+export { default as MoodTracker } from "./MoodTracker";
+export { default as PeriodNavigator } from "./PeriodNavigator";
+export { default as SummaryCards } from "./SummaryCards";
+export { default as BillsProgressCard } from "./BillsProgressCard";
+export { default as TotalSavingsCard } from "./TotalSavingsCard";
+export { default as SavingsBuckets } from "./SavingsBuckets";
+export { default as DashboardFAB } from "./DashboardFAB";
