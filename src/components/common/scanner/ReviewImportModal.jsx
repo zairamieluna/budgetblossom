02791b3ac74a4503b728159302d282547a0b0c06
zairamieluna <zairamie.luna@gmail.com>
2026-06-26 20:46:25 -0400

    Create ReviewImportModal.jsx

---
+++ src/components/common/scanner/ReviewImportModal.jsx
@@ -0,0 +1,10 @@
+{
+  "documentType": "credit-card",
+  "fields": {
+    "balance": 1497,
+    "creditLimit": 2500,
+    "availableCredit": 1003,
+    "minimumPayment": 50,
+    "dueDate": "July 14"
+  }
+}
